0396de601dfbea3f78caf393208e146f38913b56
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    mojito: Adding GSI keys
    Bug: 112293933
    Test: tree hugger
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -10,6 +10,9 @@ $(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/mojito/mojito-vendor.mk)
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
